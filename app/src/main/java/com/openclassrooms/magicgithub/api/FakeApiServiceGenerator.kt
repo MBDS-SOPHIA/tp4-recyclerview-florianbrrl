ce61af303c0597a4209c58ec3bfe6e21f766dc26
Florian Barrali <barrali.florian@gmail.com>
2025-01-06 13:29:00 +0100

    Etape 2: Gestion des images de profile

--- app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiServiceGenerator.kt
+++ app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiServiceGenerator.kt
@@ -7,34 +7,34 @@ object FakeApiServiceGenerator {
 
     @JvmField
     var FAKE_USERS = mutableListOf(
-        User("001", "Jake", "https://api.adorable.io/AVATARS/512/1.png"),
-        User("002", "Paul", "https://api.adorable.io/AVATARS/512/2.png"),
-        User("003", "Phil", "https://api.adorable.io/AVATARS/512/3.png"),
-        User("004", "Guillaume", "https://api.adorable.io/AVATARS/512/4.png"),
-        User("005", "Francis", "https://api.adorable.io/AVATARS/512/5.png"),
-        User("006", "George", "https://api.adorable.io/AVATARS/512/6.png"),
-        User("007", "Louis", "https://api.adorable.io/AVATARS/512/7.png"),
-        User("008", "Mateo", "https://api.adorable.io/AVATARS/512/8.png"),
-        User("009", "April", "https://api.adorable.io/AVATARS/512/9.png"),
-        User("010", "Louise", "https://api.adorable.io/AVATARS/512/10.png"),
-        User("011", "Elodie", "https://api.adorable.io/AVATARS/512/11.png"),
-        User("012", "Helene", "https://api.adorable.io/AVATARS/512/12.png"),
-        User("013", "Fanny", "https://api.adorable.io/AVATARS/512/13.png"),
-        User("014", "Laura", "https://api.adorable.io/AVATARS/512/14.png"),
-        User("015", "Gertrude", "https://api.adorable.io/AVATARS/512/15.png"),
-        User("016", "Chloé", "https://api.adorable.io/AVATARS/512/16.png"),
-        User("017", "April", "https://api.adorable.io/AVATARS/512/17.png"),
-        User("018", "Marie", "https://api.adorable.io/AVATARS/512/18.png"),
-        User("019", "Henri", "https://api.adorable.io/AVATARS/512/19.png"),
-        User("020", "Rémi", "https://api.adorable.io/AVATARS/512/20.png")
+        User("001", "Jake", "https://robohash.org/Jake.png"),
+        User("002", "Paul", "https://robohash.org/Paul.png"),
+        User("003", "Phil", "https://robohash.org/Phil.png"),
+        User("004", "Guillaume", "https://robohash.org/Guillaume.png"),
+        User("005", "Francis", "https://robohash.org/Francis.png"),
+        User("006", "George", "https://robohash.org/George.png"),
+        User("007", "Louis", "https://robohash.org/Louis.png"),
+        User("008", "Mateo", "https://robohash.org/Mateo.png"),
+        User("009", "April", "https://robohash.org/April.png"),
+        User("010", "Louise", "https://robohash.org/Louise.png"),
+        User("011", "Elodie", "https://robohash.org/Elodie.png"),
+        User("012", "Helene", "https://robohash.org/Helene.png"),
+        User("013", "Fanny", "https://robohash.org/Fanny.png"),
+        User("014", "Laura", "https://robohash.org/Laura.png"),
+        User("015", "Gertrude", "https://robohash.org/Gertrude.png"),
+        User("016", "Chloé", "https://robohash.org/Chloé.png"),
+        User("017", "April", "https://robohash.org/April.png"),
+        User("018", "Marie", "https://robohash.org/Marie.png"),
+        User("019", "Henri", "https://robohash.org/Henri.png"),
+        User("020", "Rémi", "https://robohash.org/Rémi.png")
     )
 
     @JvmField
     var FAKE_USERS_RANDOM = Arrays.asList(
-        User("021", "Lea", "https://api.adorable.io/AVATARS/512/21.png"),
-        User("022", "Geoffrey", "https://api.adorable.io/AVATARS/512/22.png"),
-        User("023", "Simon", "https://api.adorable.io/AVATARS/512/23.png"),
-        User("024", "André", "https://api.adorable.io/AVATARS/512/24.png"),
-        User("025", "Leopold", "https://api.adorable.io/AVATARS/512/25.png")
+        User("021", "Lea", "https://robohash.org/Lea.png"),
+        User("022", "Geoffrey", "https://robohash.org/Geoffrey.png"),
+        User("023", "Simon", "https://robohash.org/Simon.png"),
+        User("024", "André", "https://robohash.org/André.png"),
+        User("025", "Leopold", "https://robohash.org/Leopold.png")
     )
 }
